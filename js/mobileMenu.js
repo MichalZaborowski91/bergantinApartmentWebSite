a650237bb9bf7050345f9d2ed531c079323dbc92
Michal <mkzaborowski@gmail.com>
2024-03-07 22:02:18 +0000

    mobile menu added

---
+++ js/mobileMenu.js
@@ -0,0 +1,14 @@
+"use strict";
+const mobileMenuBtn = document.querySelector(".header__mobileBtn");
+const modal = document.querySelector(".mobileMenu");
+const closeBtn = document.querySelector(".close");
+
+mobileMenuBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+});
