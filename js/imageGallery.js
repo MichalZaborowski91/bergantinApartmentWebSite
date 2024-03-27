8587d3c387673ea7de518a992d41701ea7f7a019
Michal <mkzaborowski@gmail.com>
2024-03-27 23:32:10 +0000

    lightbox added to gallery

---
+++ js/imageGallery.js
@@ -0,0 +1,99 @@
+const lightboxEnabled = document.querySelectorAll(".lightboxEnabled");
+const lightboxContainer = document.querySelector(".lightboxContainer");
+const lightboxImage = document.querySelector(".lightboxImage");
+const lightboxBtns = document.querySelectorAll(".lightboxBtn");
+const lightboxBtnLeft = document.querySelector("#left");
+const lightboxBtnRight = document.querySelector("#right");
+const lightboxArray = Array.from(lightboxEnabled);
+let activeImage = 0;
+
+const showLightbox = () => {
+  lightboxContainer.classList.add("active");
+  hideHeaders();
+  setActiveImage(activeImage);
+};
+
+const hideLightbox = () => {
+  lightboxContainer.classList.remove("active");
+  showHeaders();
+};
+
+const hideHeaders = () => {
+  document.querySelectorAll(".imagesGallery__header").forEach((header) => {
+    header.style.display = "none";
+  });
+};
+
+const showHeaders = () => {
+  document.querySelectorAll(".imagesGallery__header").forEach((header) => {
+    header.style.display = "block";
+  });
+};
+
+const setActiveImage = (index) => {
+  lightboxImage.src = lightboxArray[index].getAttribute("data-imagesrc");
+  activeImage = index;
+  updateButtons();
+};
+
+const updateButtons = () => {
+  lightboxBtnLeft.classList.toggle("inactive", activeImage === 0);
+  lightboxBtnRight.classList.toggle(
+    "inactive",
+    activeImage === lightboxArray.length - 1
+  );
+};
+
+const removeBtnAnimation = () => {
+  lightboxBtns.forEach((btn) => {
+    btn.blur();
+  });
+};
+
+lightboxEnabled.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    activeImage = index;
+    showLightbox();
+  });
+});
+
+lightboxContainer.addEventListener("click", () => {
+  hideLightbox();
+});
+
+lightboxBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (!btn.classList.contains("inactive")) {
+      activeImage += btn.id === "left" ? -1 : 1;
+      setActiveImage(activeImage);
+      removeBtnAnimation();
+    }
+  });
+});
+
+window.addEventListener("keydown", (e) => {
+  if (!lightboxContainer.classList.contains("active")) return;
+  if (e.key === "ArrowLeft") {
+    let prevIndex = activeImage - 1;
+    while (prevIndex >= 0 && lightboxArray[prevIndex].nodeName === "H2") {
+      prevIndex--;
+    }
+    if (prevIndex >= 0) {
+      activeImage = prevIndex;
+      setActiveImage(activeImage);
+    }
+  } else if (e.key === "ArrowRight") {
+    let nextIndex = activeImage + 1;
+    while (
+      nextIndex < lightboxArray.length &&
+      lightboxArray[nextIndex].nodeName === "H2"
+    ) {
+      nextIndex++;
+    }
+    if (nextIndex < lightboxArray.length) {
+      activeImage = nextIndex;
+      setActiveImage(activeImage);
+    }
+  }
+});
