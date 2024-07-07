8ca6b5ba1d5d17b407292a78659b228b717e287b
Michal <mkzaborowski@gmail.com>
2024-07-08 00:27:13 +0100

    language updates

--- js/language.js
+++ js/language.js
@@ -3,7 +3,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   function changeLanguage(event) {
     const selectedLanguageUrl = event.currentTarget.getAttribute("data-url");
+    //Add language to localStorage
+    const selectedLanguage = event.currentTarget.getAttribute("data-language");
+    localStorage.setItem("selectedLanguage", selectedLanguage);
+    //Set language
     window.location.href = selectedLanguageUrl;
+  }
+
+  //Check if there is language in localStorage and if website was checked => sessionStorage
+  const selectedLanguage = localStorage.getItem("selectedLanguage");
+  const languageChecked = sessionStorage.getItem("languageChecked");
+
+  if (selectedLanguage && !languageChecked) {
+    //Map selected language to URL
+    const languageUrlMap = {
+      en: "index.html",
+      pl: "./languages/pl/index_pl.html",
+      de: "./languages/de/index_de.html",
+      es: "./languages/es/index_es.html",
+      fr: "./languages/fr/index_fr.html",
+    };
+
+    const selectedLanguageUrl = languageUrlMap[selectedLanguage];
+
+    //Mark that language was checked in sessionStorage
+    sessionStorage.setItem("languageChecked", "true");
+
+    //Transfer to language version
+    if (
+      selectedLanguageUrl &&
+      !window.location.href.includes(selectedLanguageUrl)
+    ) {
+      window.location.href = selectedLanguageUrl;
+    }
   }
 
   // Select all elements with the class 'languageSelect'
