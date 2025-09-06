151250d76bf51cef9db4b8a9dd3d261e823f8f4b
Michal <mkzaborowski@gmail.com>
2025-09-06 01:07:35 +0100

    updates 2025

--- js/contactWidget.js
+++ js/contactWidget.js
@@ -7,30 +7,53 @@ const widgetPhone = document.querySelector(".widgetPhone");
 
 let isWidgetActive = false;
 
+const showWidgets = () => {
+  widgetEmail.classList.add("activateWidgetEmail");
+  widgetEmail.classList.remove("deactivateWidgetEmail");
+  setTimeout(() => {
+    widgetWhatsapp.classList.add("activateWidgetWhatsapp");
+    widgetWhatsapp.classList.remove("deactivateWidgetWhatsapp");
+  }, 200);
+  setTimeout(() => {
+    widgetPhone.classList.add("activateWidgetPhone");
+    widgetPhone.classList.remove("deactivateWidgetPhone");
+  }, 400);
+  isWidgetActive = true;
+};
+
+const hideWidgets = () => {
+  widgetPhone.classList.remove("activateWidgetPhone");
+  widgetPhone.classList.add("deactivateWidgetPhone");
+  setTimeout(() => {
+    widgetWhatsapp.classList.remove("activateWidgetWhatsapp");
+    widgetWhatsapp.classList.add("deactivateWidgetWhatsapp");
+  }, 200);
+  setTimeout(() => {
+    widgetEmail.classList.remove("activateWidgetEmail");
+    widgetEmail.classList.add("deactivateWidgetEmail");
+  }, 400);
+  isWidgetActive = false;
+};
+
 const widgetHandler = () => {
   if (!isWidgetActive) {
-    widgetEmail.classList.add("activateWidgetEmail");
-    widgetEmail.classList.remove("deactivateWidgetEmail");
-    setTimeout(() => {
-      widgetWhatsapp.classList.add("activateWidgetWhatsapp");
-      widgetWhatsapp.classList.remove("deactivateWidgetWhatsapp");
-    }, 200);
-    setTimeout(() => {
-      widgetPhone.classList.add("activateWidgetPhone");
-      widgetPhone.classList.remove("deactivateWidgetPhone");
-    }, 400);
+    showWidgets();
   } else {
-    widgetPhone.classList.remove("activateWidgetPhone");
-    widgetPhone.classList.add("deactivateWidgetPhone");
-    setTimeout(() => {
-      widgetWhatsapp.classList.remove("activateWidgetWhatsapp");
-      widgetWhatsapp.classList.add("deactivateWidgetWhatsapp");
-    }, 200);
-    setTimeout(() => {
-      widgetEmail.classList.remove("activateWidgetEmail");
-      widgetEmail.classList.add("deactivateWidgetEmail");
-    }, 400);
+    hideWidgets();
   }
-  isWidgetActive = !isWidgetActive;
 };
+
 contactWidget.addEventListener("click", widgetHandler);
+
+contactWidget.addEventListener("blur", () => {
+  if (isWidgetActive) {
+    hideWidgets();
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && isWidgetActive) {
+    hideWidgets();
+    contactWidget.blur();
+  }
+});
